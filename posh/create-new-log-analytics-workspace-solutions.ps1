a940836d50dad4f73f7090f1d93b27216764318a
Shannon Eldridge-Kuehn <shannon.kuehn@gmail.com>
2025-02-02 20:40:59 -0600

    Update create-new-log-analytics-workspace-solutions.ps1

--- posh/create-new-log-analytics-workspace-solutions.ps1
+++ posh/create-new-log-analytics-workspace-solutions.ps1
@@ -1,25 +1,42 @@
-﻿[CmdletBinding()]
-    param(
+[CmdletBinding()]
+param(
     [Parameter(Mandatory=$true)]
     [string]$location,
     [Parameter(Mandatory=$true)]
     [string]$workspaceName,
     [Parameter(Mandatory=$true)]
     [string]$resourceGroupName
-    )
+)
 
-New-AzResourceGroup -Location $location -Name $resourceGroupName
-New-AzOperationalInsightsWorkspace -Location $location `
-                                   -Name $workspaceName `
-                                   -Sku PerGB2018 `
-                                   -ResourceGroupName $resourceGroupName
+# Connect to Azure
+Connect-AzAccount
 
-$solutions = @('ADAssessment','AzureActivity','AzureNetworking','AgentHealthAssessment','InfrastructureInsights','KeyVaultAnalytics','NetworkMonitoring','AntiMalware',`
-'AlertManagement','Security','ContainerInsights','Containers','DHCPActivity','DnsAnalytics','ServiceMap','SiteRecovery','SecurityCenterFree','SecurityInsights',`
-'VMInsights','AgentHealthAssessment')
-
-foreach ($solution in $solutions) {
-    Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -IntelligencePackName $solution -Enabled $true
+# Create Resource Group if it doesn't exist
+if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
+    New-AzResourceGroup -Location $location -Name $resourceGroupName
 }
 
+# Create Log Analytics Workspace
+$workspace = New-AzOperationalInsightsWorkspace -Location $location `
+                                                -Name $workspaceName `
+                                                -Sku PerGB2018 `
+                                                -ResourceGroupName $resourceGroupName
 
+# List of solutions to add to the Log Analytics Workspace
+$solutions = @(
+    'ADAssessment','AzureActivity','AzureNetworking','AgentHealthAssessment',
+    'InfrastructureInsights','KeyVaultAnalytics','NetworkMonitoring','AntiMalware',
+    'AlertManagement','Security','ContainerInsights','Containers','DHCPActivity',
+    'DnsAnalytics','ServiceMap','SiteRecovery','SecurityCenterFree','SecurityInsights',
+    'VMInsights','AgentHealthAssessment'
+)
+
+# Add solutions to the Log Analytics Workspace
+foreach ($solution in $solutions) {
+    Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $resourceGroupName `
+                                              -WorkspaceName $workspaceName `
+                                              -IntelligencePackName $solution `
+                                              -Enabled $true
+}
+
+Write-Host "Log Analytics workspace and solutions have been created successfully."
